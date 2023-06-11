007b1270037cae300f9a08409e5f0a1b92fcc496
Maruo.S <raspi-maru2004@outlook.jp>
2023-06-12 00:41:57 +0900

    add imports

--- sort/__init__.py
+++ sort/__init__.py
@@ -2,4 +2,6 @@ from .bubble import bubble
 from .insert import insert
 from .selecting import select
 from .shell import shell
+from .quick import quick
+from .merge import merge
 from .util import isSorted
